fdce7eee2cbb59f82ebaea5d76bffae3be552d34
abdel hamed <vfor12@yahoo.com>
2025-04-04 01:30:57 +0200

    add layout components and integrate Tailwind CSS styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+          { path: 'Home', loadComponent: () => import('./pages/main/main.component').then(m => m.MainComponent) },
+          { path: '**', redirectTo: 'Home' }
+        ]
+      }
+];
